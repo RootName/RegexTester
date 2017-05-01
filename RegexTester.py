869aa414ae789996e60c5caee47b1fc5c8f040e7
Black Viking <yakuza3003@yandex.com>
2017-05-01 14:39:55 +0300

    RegexTester.py

---
+++ RegexTester.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#
+#
+
+__author__ = "Black Viking"
+__date     = "01.05.2017"
+
+from Tkinter import *
+import re
+
+def test(string, reString):
+
+	reString = re.compile(reString)
+	result   = re.findall(reString, string)
+
+	textRes.delete('1.0', END)
+
+	for res in result:
+		textRes.insert(END, res+"\n")
+
+	textRes.insert(END, "-"*40+"\n")
+
+
+def main():
+	global textRes, entryStr, entryRe
+
+	root       = Tk()
+	root.title = "Black Viking | Regex Tester"
+	root.tk_setPalette("black")
+
+	labelStr = Label(text="String :", fg="green")
+	labelRe  = Label(text="Regex  :", fg="green")
+	labelRes = Label(text="Result :", fg="green")
+
+	entryStr = Entry()
+	entryRe  = Entry()
+
+	textRes  = Text(fg="green")
+
+	button   = Button(text="Test", fg="green", command = lambda: test(entryStr.get(), entryRe.get()))
+
+	labelStr.pack()
+	entryStr.pack()
+
+	labelRe.pack()
+	entryRe.pack()
+
+	labelRes.pack()
+	textRes.pack()
+
+	button.pack()
+
+	root.mainloop()
+
+if __name__ == "__main__":
+	main()
